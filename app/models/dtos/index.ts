b3e061509402dc4799fefda311366c877268f33f
Lasse Nitschke Duer <lasse2100@gmail.com>
2024-10-15 23:44:22 +0200

    feat: add leaderboard page

--- app/models/dtos/index.ts
+++ app/models/dtos/index.ts
@@ -2,3 +2,4 @@ export * from "./post-dto.model";
 export * from "./new-post-dto.model";
 export * from "./create-post-response-dto.model";
 export * from "./user-dto.model";
+export * from "./post-leaderboard-dto.model";
